081f64e60dbebe6598a08187e6a4f49092a74990
ax <465559733@qq.com>
2019-02-20 10:25:09 +0800

    修改文件配置

--- app/vendor.ts
+++ app/vendor.ts
@@ -1,9 +1,10 @@
 
-// Angular 2
+// Angular 7
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
+import '@angular/compiler';
 import '@angular/forms';
 import '@angular/router';
 import '@angular/upgrade';
